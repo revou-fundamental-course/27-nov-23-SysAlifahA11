b895a0b3bfa4ae4b9a5187bb8a60e2d9d023f6a4
unknown <alifahalfathin@gmail.com>
2023-12-07 10:22:11 +0700

    push

--- js/script.js
+++ js/script.js
@@ -2,10 +2,20 @@
 const alas = document.getElementById('input-alas')
 const tinggi = document.getElementById('input-tinggi')
 const hitung = document.getElementById('button-hitung')
+const reset = document.getElementById('button-reset')
 const output = document.getElementById('output')
+
 hitung.addEventListener(`click`, function(){
     let a = alas.value
     let t = tinggi.value
     let l = 0.5*a*t
     output.innerHTML = `Luas segitiga dengan alas ${a}cm dan tinggi ${t}cm adalah ${l}cm2`
+})
+
+reset.addEventListener(`click`, function(){
+    let a = alas.value
+    let t = tinggi.value
+    alas.value = null;
+    tinggi.value = null;
+    output.innerHTML = null;
 })
