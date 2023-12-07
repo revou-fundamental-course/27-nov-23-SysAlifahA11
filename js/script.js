c67d7e5574e3a41d36c8508e2e5d3f850993d22a
unknown <alifahalfathin@gmail.com>
2023-12-07 11:02:33 +0700

    push tros

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+//luas
 
 const alas = document.getElementById('input-alas')
 const tinggi = document.getElementById('input-tinggi')
@@ -18,4 +19,32 @@ reset.addEventListener(`click`, function(){
     alas.value = null;
     tinggi.value = null;
     output.innerHTML = null;
+})
+
+//keliling
+
+const ab = document.getElementById('input-sisiab')
+const bc = document.getElementById('input-sisibc')
+const ca = document.getElementById('input-sisica')
+const hitung1 = document.getElementById('button-hitung1')
+const reset1 = document.getElementById('button-reset1')
+const output1 = document.getElementById('output1')
+
+hitung1.addEventListener(`click`, function(){
+    let s1 = ab.value
+    let s2 = bc.value
+    let s3 = ca.value
+    let k = s1+s2+s3
+    output1.innerHTML = `Keliling segitiga dengan sisi ${s1}cm, ${s2}cm, dan ${s3}cm adalah ${k}cm`
+})
+
+reset1.addEventListener(`click`, function(){
+    let s1 = ab.value
+    let s2 = bc.value
+    let s3 = ca.value
+    let k = s1+s2+s3
+    ab.value = null;
+    bc.value = null;
+    ca.value = null;
+    output1.innerHTML = null;
 })
